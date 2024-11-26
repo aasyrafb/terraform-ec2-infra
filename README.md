c66bb079fce4f8d98ebc117c9b7d1523cb5542b5
aasyrafb <amirulasyrafbahari@gmail.com>
2024-11-26 22:52:52 +0800

    terraform code for ec2

--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 # terraform-ec2-infra
 terraform apply -var="name=aasyrafbb" -var="vpc_id=vpc-067f3ab097282bc4d" -var="subnet_id=subnet-0021081c508245985"
 
-terraform apply 0var-file=dev.tfvars
+terraform apply -var-file="dev.tfvars"
+
+terraform output
+terraform output -raw public_ip
+terraform output -raw public_dns
+terraform output -raw public.ids
+terraform output -raw private.ids
 ```
--- main.tf
+++ main.tf
@@ -11,9 +11,34 @@ variable "vpc_id" {
 }
 
 variable "subnet_id" {
-    description = "vpc id"
+    description = "subnet id"
     type = string
-    #default = "subnet-0021081c508245985"
+    #default = "vpc-067f3ab097282bc4d"
+}
+
+data "aws_subnets" "selected" {
+    filter {
+        name = "vpc-id"
+        values = [var.vpc_id]
+    }
+}
+
+data "aws_subnets" "public" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
+}
+
+locals {
+    department = "marketing"
 }
 
 resource "aws_instance" "public" {
@@ -25,14 +50,18 @@ resource "aws_instance" "public" {
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
  
   tags = {
-    Name = "${var.name}-ec2"    #Prefix your own name, e.g. jazeel-ec2
+    Name = "${var.name}-ec2"
+    Department = local.department    #Prefix your own name, e.g. jazeel-ec2
   }
 }
 
 resource "aws_security_group" "allow_ssh" {
   name        = "${var.name}-sg" #Security group name, e.g. jazeel-terraform-security-group
   description = "Allow SSH inbound"
-  vpc_id      = var.vpc_id  #VPC ID (Same VPC as your EC2 subnet above), E.g. vpc-xxxxxxx
+  vpc_id      = var.vpc_id 
+  lifecycle {
+    create_before_destroy = true
+  } #VPC ID (Same VPC as your EC2 subnet above), E.g. vpc-xxxxxxx
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
@@ -41,4 +70,20 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
+}
+
+output "public_ip" {
+    value = aws_instance.public.public_ip
+}
+
+output "public_dns" {
+    value = aws_instance.public.public_dns
+}
+
+output "public_subnet_ids" {
+    value = data.aws_subnets.public.ids
+}
+
+output "private_subnet_ids" {
+    value = data.aws_subnets.private.ids
 }
