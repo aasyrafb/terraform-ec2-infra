e0c1ed03e4e93855cb270b937498933c8beee534
aasyrafb <amirulasyrafbahari@gmail.com>
2024-11-26 21:13:27 +0800

    terraform code for ec2

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+```bash
 # terraform-ec2-infra
+terraform apply -var="name=aasyrafbb" -var="vpc_id=vpc-067f3ab097282bc4d" -var="subnet_id=subnet-0021081c508245985"
+
+terraform apply 0var-file=dev.tfvars
+```
--- main.tf
+++ main.tf
@@ -1,25 +1,38 @@
-#variable "name" {
- #   description = "name of application"
-  #  type = string
-  #  default = "asyraf"
-#}
+variable "name" {
+    description = "name of application"
+    type = string
+    #default = "asyraf-with-prefix"
+}
+
+variable "vpc_id" {
+    description = "vpc id"
+    type = string
+    #default = "vpc-067f3ab097282bc4d"
+}
+
+variable "subnet_id" {
+    description = "vpc id"
+    type = string
+    #default = "subnet-0021081c508245985"
+}
+
 resource "aws_instance" "public" {
   ami                         = "ami-04c913012f8977029"
   instance_type               = "t2.micro"
-  subnet_id                   = "subnet-0021081c508245985"  #Public Subnet ID, e.g. subnet-xxxxxxxxxxx
+  subnet_id                   = var.subnet_id  #Public Subnet ID, e.g. subnet-xxxxxxxxxxx
   associate_public_ip_address = true
   key_name                    = "asyraf-kp" #Change to your keyname, e.g. jazeel-key-pair
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
  
   tags = {
-    Name = "asyraf-ec2"    #Prefix your own name, e.g. jazeel-ec2
+    Name = "${var.name}-ec2"    #Prefix your own name, e.g. jazeel-ec2
   }
 }
 
 resource "aws_security_group" "allow_ssh" {
-  name        = "asyraf-terraform-sg" #Security group name, e.g. jazeel-terraform-security-group
+  name        = "${var.name}-sg" #Security group name, e.g. jazeel-terraform-security-group
   description = "Allow SSH inbound"
-  vpc_id      = "vpc-067f3ab097282bc4d"  #VPC ID (Same VPC as your EC2 subnet above), E.g. vpc-xxxxxxx
+  vpc_id      = var.vpc_id  #VPC ID (Same VPC as your EC2 subnet above), E.g. vpc-xxxxxxx
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
